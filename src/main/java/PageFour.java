33f14f34e41b1e53208d7b21ac0a1c75ae1db6ae
mmamyt2 <mmamyt2@uic.edu>
2022-04-19 10:14:12 -0600

    Initial commit

---
+++ src/main/java/PageFour.java
@@ -0,0 +1,173 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.time.temporal.TemporalAdjusters;
+
+import static java.time.LocalDate.now;
+
+public class PageFour extends javax.swing.JFrame{
+    public JFrame frame;
+    public JPanel panel;
+    public JLabel labelID, labelFN, labelLN, labelRole, labelSDate, labelEDate, labelSalary, labelDepartment, labelAddress, labelEmail;
+    public JTextField ID, firstName, lastName, role, startDate, endDate, salary, department, Address, email;
+    public javax.swing.JButton buttonUpdate, buttonGenerate;
+
+    public PageFour(){
+        frame = new JFrame();
+        panel = new JPanel();
+        frame.setSize(700, 450);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        panel.setLayout(null);
+
+        //ID
+        labelID = new JLabel("ID:");
+        labelID.setBounds(20, 40, 100, 25);
+        panel.add(labelID);
+        ID = new JTextField();
+        ID.setBounds(100, 40, 150, 25);
+        panel.add(ID);
+
+        //First Name
+        labelFN = new JLabel("First Name:");
+        labelFN.setBounds(20, 70, 100, 25);
+        panel.add(labelFN);
+        firstName = new JTextField();
+        firstName.setBounds(100, 70, 150, 25);
+        panel.add(firstName);
+
+        //Last Name
+        labelLN = new JLabel("Last Name:");
+        labelLN.setBounds(20, 100, 100, 25);
+        panel.add(labelLN);
+        lastName = new JTextField();
+        lastName.setBounds(100, 100, 150, 25);
+        panel.add(lastName);
+
+        //Address
+        labelAddress = new JLabel("Address:");
+        labelAddress.setBounds(20, 130, 100, 25);
+        panel.add(labelAddress);
+        Address = new JTextField();
+        Address.setBounds(100, 130, 150, 25);
+        panel.add(Address);
+
+        //Start date
+        labelSDate = new JLabel("Start Date:");
+        labelSDate.setBounds(20, 160, 100, 25);
+        panel.add(labelSDate);
+        startDate = new JTextField();
+        startDate.setBounds(100, 160, 150, 25);
+        panel.add(startDate);
+
+        //End Date
+        labelEDate = new JLabel("End Date:");
+        labelEDate.setBounds(300, 40, 100, 25);
+        panel.add(labelEDate);
+        endDate = new JTextField();
+        endDate.setBounds(380, 40, 150, 25);
+        panel.add(endDate);
+
+        //Role
+        labelRole = new JLabel("Role:");
+        labelRole.setBounds(300, 70, 100, 25);
+        panel.add(labelRole);
+        role = new JTextField();
+        role.setBounds(380, 70, 150, 25);
+        panel.add(role);
+
+        //Department
+        labelDepartment = new JLabel("Department:");
+        labelDepartment.setBounds(300, 100, 100, 25);
+        panel.add(labelDepartment);
+        department = new JTextField();
+        department.setBounds(380, 100, 150, 25);
+        panel.add(department);
+
+        //Salary
+        labelSalary = new JLabel("Salary:");
+        labelSalary.setBounds(300, 130, 100, 25);
+        panel.add(labelSalary);
+        salary = new JTextField();
+        salary.setBounds(380, 130, 150, 25);
+        panel.add(salary);
+
+        //email
+        labelEmail = new JLabel("email:");
+        labelEmail.setBounds(300, 160, 100, 25);
+        panel.add(labelEmail);
+        email = new JTextField();
+        email.setBounds(380, 160, 150, 25);
+        panel.add(email);
+
+        //button
+        buttonUpdate = new JButton("Update Record");
+        buttonUpdate.setBounds(380, 220, 150, 25);
+        panel.add(buttonUpdate);
+        buttonUpdate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    String query = "UPDATE Employees " +
+                            "SET First_Name = '"+ firstName.getText() + "', Last_Name = '" + lastName.getText() + "', role ='" + role.getText() +
+                            "', StartDate = '" + startDate.getText() + "', EndDate = '" + endDate.getText() + "', salary = "+
+                            salary.getText() + ", department = '" + department.getText() + "', Address = '" + Address.getText() + "', email ='" + email.getText() +"' WHERE id ="+ ID.getText();
+                    Connection c4 = getConnection();
+                    Statement s4 = c4.createStatement();
+                    int rowCount = s4.executeUpdate(query);
+                }catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+        //button to generate Payslip
+        buttonGenerate = new JButton("Create Payslip");
+        buttonGenerate.setBounds(380, 250, 150, 25);
+        panel.add(buttonGenerate);
+        buttonGenerate.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try{
+                    String path = "C:\\Users\\mmamyt2\\Desktop\\"+firstName.getText()+".pdf";
+                    String text = "                  PAYSLIP \n" +
+                            ""+
+                            "\nPay Date:       "+ now() +
+                            "\n Pay Period:    "+ now().minusMonths(1).with(TemporalAdjusters.firstDayOfMonth())+ " - " + now().minusMonths(1).with(TemporalAdjusters.lastDayOfMonth()) + "\n"+
+                            "\nFull Name:      "+ firstName.getText() + " " + lastName.getText() +
+                            "\n Address:       " + Address.getText() +
+                            "\n Salary:        LTC " + salary.getText();
+                    Paragraph par = new Paragraph(text);
+                    PdfWriter pdfWriter = new PdfWriter(path);
+                    PdfDocument pdfDoc = new PdfDocument(pdfWriter);
+                    pdfDoc.addNewPage();
+                    Document document = new Document(pdfDoc);
+                    document.add(par);
+                    document.close();
+                } catch (Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+
+        frame.setVisible(true);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        String dbUrl = "jdbc:sqlite:C:\\SQLiteStudio\\PayrollSystem";
+        Connection connection = DriverManager.getConnection(dbUrl);
+        return connection;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        PageFour pf = new PageFour();
+
+    }
+}
---
+++ src/main/java/Payroll_GUI.java
@@ -0,0 +1,143 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Payroll_GUI extends JFrame {
+
+    private JFrame loginPage = new JFrame("PayHero");
+    private String username = "Admin";
+    private String password = "Admin123";
+    private JTextField usernameText = new JTextField(20);
+    private JTextField passwordText = new JTextField(20);
+    private JButton loginBTN = new JButton("Login");
+    HomePageGUI hpgui;
+
+    public Payroll_GUI()
+    {
+        //setTitle("PayHero");
+        //Container cp = getContentPane();
+        JPanel titlePanel = new JPanel();
+        JPanel bodyPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10,10,10,10);
+        //cp.setLayout(new BorderLayout());
+        loginPage.setLayout(new BorderLayout());
+        loginPage.setMinimumSize(new Dimension(700,450));
+        loginPage.setLocationByPlatform(true);
+        loginPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel payHeroTitle = new JLabel("PayHero");
+        payHeroTitle.setFont(new Font("Dark Future", Font.BOLD, 34));
+        titlePanel.add(payHeroTitle);
+
+        JLabel usernameLabel = new JLabel("Username: ");
+        c.gridx = 0;
+        c.gridy = 0;
+        bodyPanel.add(usernameLabel, c);
+
+
+        usernameText.setEditable(true);
+        c.gridx = 1;
+        bodyPanel.add(usernameText, c);
+
+        JLabel passwordLabel = new JLabel("Password: ");
+        c.gridx = 0;
+        c.gridy = 1;
+        bodyPanel.add(passwordLabel, c);
+
+        passwordText.setEditable(true);
+        c.gridx = 1;
+        bodyPanel.add(passwordText, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 2;
+        c.anchor = GridBagConstraints.CENTER;
+        bodyPanel.add(loginBTN, c);
+
+        ActionListener1 aListen = new ActionListener1();
+        loginBTN.addActionListener(aListen);
+
+        loginPage.add(titlePanel, BorderLayout.NORTH);
+        loginPage.add(bodyPanel, BorderLayout.CENTER);
+
+        loginPage.setVisible(true);
+    }
+
+    class ActionListener1 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent evt)
+        {
+            if(usernameText.getText().contains(username) && passwordText.getText().contains(password))
+            {
+                loginPage.dispose();
+                new HomePageGUI();
+            }
+        }
+    }
+}
+
+class HomePageGUI extends JFrame{
+
+    private JFrame homePage = new JFrame("PayHero");
+    private JTextField sEIDText = new JTextField(15);
+    private JButton searchBTN = new JButton("Search");
+    private JButton newEmpBTN = new JButton("New Employee Record");
+
+    public HomePageGUI()
+    {
+        //setTitle("Home Page");
+        //Container cp = getContentPane();
+        //cp.setLayout(new BorderLayout());
+        homePage.setLayout(new BorderLayout());
+        JPanel centerPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10,10,10,10);
+        homePage.setMinimumSize(new Dimension(700,450));
+        homePage.setLocationByPlatform(true);
+        homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        sEIDText.setEditable(true);
+        c.gridx = 0;
+        c.gridy = 0;
+        centerPanel.add(sEIDText);
+
+        c.gridx = 1;
+        centerPanel.add(searchBTN);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        centerPanel.add(newEmpBTN, c);
+
+
+        ActionListener2 aListen2 = new ActionListener2();
+        searchBTN.addActionListener(aListen2);
+        newEmpBTN.addActionListener(aListen2);
+
+        homePage.add(centerPanel, BorderLayout.CENTER);
+        homePage.setVisible(true);
+    }
+
+    class ActionListener2 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent evt)
+        {
+            if(evt.getSource() == searchBTN)
+            {
+                homePage.dispose();
+                PageFour pf = new PageFour();
+            }
+
+            if(evt.getSource() == newEmpBTN)
+            {
+                homePage.dispose();
+                PageThree pt = new PageThree();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new Payroll_GUI();
+    }
+}
